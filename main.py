e301a9308af3fb90bcf80f0bb572c2fcf4b15e05
Antut <daneluzzi.anthony@gmail.com>
2025-04-17 00:58:02 +0200

    change scale to 3.3 and int

--- main.py
+++ main.py
@@ -58,10 +58,10 @@ while True:
     line_size = 10
 
     # Upscale the image using new  width and height
-    resize = 3
+    resize = 3.33333333
     up_width = 160 * resize
     up_height = 120 * resize
-    up_points = (up_width, up_height)
+    up_points = (int(up_width), int(up_height))
     resized_up = cv2.resize(frame, up_points, interpolation=cv2.INTER_LANCZOS4)
     #Remove noise with blur then Sharpen it
     smoothed = cv2.GaussianBlur(resized_up, (3, 3), 10)
