d70dab749841bff2222256d2fa7d578748d67b55
Antut <daneluzzi.anthony@gmail.com>
2025-04-17 01:34:20 +0200

    remove move windows

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ while True:
 
     winname = "flir_windows"
     cv2.namedWindow(winname, cv2.WINDOW_NORMAL)
-    cv2.moveWindow(winname, dim_x - 1, dim_y - 1)
+    #cv2.moveWindow(winname, dim_x - 1, dim_y - 1)
     cv2.setWindowProperty(winname, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
     # Upscale the image using new  width and height
