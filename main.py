56ea59d0c92d78479f6eb2837700f189b3c0e34d
Antut <daneluzzi.anthony@gmail.com>
2025-04-17 02:13:58 +0200

    change color and thickness of text

--- main.py
+++ main.py
@@ -89,12 +89,12 @@ while True:
     cv2.line(rgb_img, (hot_x, hot_y - line_size), (hot_x, hot_y + line_size), (255, 255, 255), 1)
     cv2.line(rgb_img, (hot_x - line_size, hot_y), (hot_x + line_size, hot_y), (255, 255, 255), 1)
     cv2.circle(rgb_img, (hot_x, hot_y), 5, (255, 255, 255), 1)
-    cv2.putText(rgb_img, max_temp_text, (hot_x+20, hot_y+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 128, 128), 2)
+    cv2.putText(rgb_img, max_temp_text, (hot_x+20, hot_y+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (128, 128, 255), 1)
 
     cv2.line(rgb_img, (cold_x, cold_y - line_size), (cold_x, cold_y + line_size), (255, 255, 255), 1)
     cv2.line(rgb_img, (cold_x - line_size, cold_y), (cold_x + line_size, cold_y), (255, 255, 255), 1)
     cv2.circle(rgb_img, (cold_x, cold_y), 5, (255, 255, 255), 1)
-    cv2.putText(rgb_img, min_temp_text, (cold_x + 20, cold_y + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (128, 128, 255), 2)
+    cv2.putText(rgb_img, min_temp_text, (cold_x + 20, cold_y + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 128, 128), 1)
 
     final_render = cv2.imshow(winname, rgb_img)
     if cv2.waitKey(1) == ord('q'):
